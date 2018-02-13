d527ab64652b009f02829ab628db426515f85503
damil84 <damil84@gmail.com>
2018-02-13 09:45:39 +0100

    init(NotebookErrorReporter)NuGet packages added, needed folders created

--- NotebookErrorReporter/NotebookErrorReporter/Startup.cs
+++ NotebookErrorReporter/NotebookErrorReporter/Startup.cs
@@ -25,10 +25,7 @@ namespace NotebookErrorReporter
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
         }
     }
 }
