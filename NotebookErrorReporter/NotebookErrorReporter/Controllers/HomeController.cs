541a9cdbad489174c78ffdea23344630aef9be55
damil84 <damil84@gmail.com>
2018-02-13 11:07:42 +0100

    feat(NotebookErrorReporter)repository and viewmodel DI added to HomeController

--- NotebookErrorReporter/NotebookErrorReporter/Controllers/HomeController.cs
+++ NotebookErrorReporter/NotebookErrorReporter/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NotebookErrorReporter.Repositories;
+using NotebookErrorReporter.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace NotebookErrorReporter.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        public Repository Repository { get; set; }
+        public NamesTicketsVM NamesTicketsVM { get; set; }
+
+        public HomeController(Repository repository, NamesTicketsVM namesTicketsVM)
+        {
+            Repository = repository;
+            NamesTicketsVM = namesTicketsVM;
+        }
+
         public IActionResult Index()
         {
             return View();
